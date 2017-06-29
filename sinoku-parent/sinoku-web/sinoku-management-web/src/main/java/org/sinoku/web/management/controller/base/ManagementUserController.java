56002f6f62c76452b250f1d8283d09a13863f030
shenao1991 <shenao19910806@outlook.com>
2017-06-29 17:32:55 +0800

    提交了pom和controller

--- sinoku-parent/sinoku-web/sinoku-management-web/src/main/java/org/sinoku/web/management/controller/base/ManagementUserController.java
+++ sinoku-parent/sinoku-web/sinoku-management-web/src/main/java/org/sinoku/web/management/controller/base/ManagementUserController.java
@@ -1,9 +1,25 @@
 package org.sinoku.web.management.controller.base;
 
+import org.sinoku.common.entity.common.ResultEntity;
+import org.sinoku.common.entity.management.ManagementUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 /**
  *
  * Created by ao.shen on 2017/6/29.
  */
+@Controller
+@RequestMapping(value = "/management/user")
 public class ManagementUserController {
+
+
+    @ResponseBody
+    @RequestMapping(value = "/adduser")
+    public ResultEntity addManagementUser(ManagementUser user){
+        return null;
+    }
+
 
 }
