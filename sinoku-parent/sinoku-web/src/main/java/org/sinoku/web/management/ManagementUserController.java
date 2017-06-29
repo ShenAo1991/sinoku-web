7aec84fd4cca5724e98e9ef65f5886802db96aa5
shenao1991 <shenao19910806@outlook.com>
2017-06-29 12:25:17 +0800

    @Author shenao 提交了更改

--- sinoku-parent/sinoku-web/src/main/java/org/sinoku/web/management/ManagementUserController.java
+++ sinoku-parent/sinoku-web/src/main/java/org/sinoku/web/management/ManagementUserController.java
@@ -1,10 +1,18 @@
 package org.sinoku.web.management;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  *  运营用户controller
  *  Created by ao.shen on 2017/6/28.
  */
-
+@Controller
+@RequestMapping(value = "/management/user")
 public class ManagementUserController {
+
+    public void helloWorld(){
+
+    }
 
 }
