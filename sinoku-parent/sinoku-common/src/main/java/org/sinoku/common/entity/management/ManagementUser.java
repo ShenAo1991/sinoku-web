39c0a824e6f5c49fbf5f879ba0188ef0b5d34bef
shenao1991 <shenao19910806@outlook.com>
2017-06-29 14:21:23 +0800

    增加了管理用户entity

---
+++ sinoku-parent/sinoku-common/src/main/java/org/sinoku/common/entity/management/ManagementUser.java
@@ -0,0 +1,77 @@
+package org.sinoku.common.entity.management;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * Created by ao.shen on 2017/6/29.
+ */
+public class ManagementUser implements Serializable{
+
+    /**主键id*/
+    private Integer id;
+
+    /**用户名*/
+    private String userName;
+
+    /**密码*/
+    private String password;
+
+    /**创建时间*/
+    private Date createTime;
+
+    /**更新时间*/
+    private Date updateTime;
+
+    /**是否删除 0否 1是*/
+    private Integer isDelete;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Integer getIsDelete() {
+        return isDelete;
+    }
+
+    public void setIsDelete(Integer isDelete) {
+        this.isDelete = isDelete;
+    }
+}
