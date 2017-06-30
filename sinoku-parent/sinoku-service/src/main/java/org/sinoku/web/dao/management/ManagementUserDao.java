49cdbef8e290e43efd47a239d6777f2519595041
shenao1991 <shenao19910806@outlook.com>
2017-06-30 15:17:56 +0800

    数据流转配置成功

--- sinoku-parent/sinoku-service/src/main/java/org/sinoku/web/dao/management/ManagementUserDao.java
+++ sinoku-parent/sinoku-service/src/main/java/org/sinoku/web/dao/management/ManagementUserDao.java
@@ -1,8 +1,17 @@
 package org.sinoku.web.dao.management;
 
+import org.sinoku.common.entity.management.ManagementUser;
+
+import java.util.List;
+
 /**
  * 运营用户Dao
  * Created by ao.shen on 2017/6/29.
  */
 public interface ManagementUserDao {
+
+    /**
+     * 获取用户
+     * */
+    public List<ManagementUser> getManagementUser();
 }
--- sinoku-parent/sinoku-web/sinoku-management-web/src/main/java/org/sinoku/web/management/controller/base/ManagementUserController.java
+++ sinoku-parent/sinoku-web/sinoku-management-web/src/main/java/org/sinoku/web/management/controller/base/ManagementUserController.java
@@ -2,9 +2,14 @@ package org.sinoku.web.management.controller.base;
 
 import org.sinoku.common.entity.common.ResultEntity;
 import org.sinoku.common.entity.management.ManagementUser;
+import org.sinoku.web.dao.management.ManagementUserDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 /**
  *
@@ -15,12 +20,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/management/user")
 public class ManagementUserController {
 
+    @Autowired
+    private ManagementUserDao dao;
+
 
     @ResponseBody
     @RequestMapping(value = "/adduser")
     public ResultEntity addManagementUser(ManagementUser user){
-        return null;
-    }
 
+        List<ManagementUser> userList = dao.getManagementUser();
+        System.out.println("111");
+        return null;
+
+    }
 
 }
