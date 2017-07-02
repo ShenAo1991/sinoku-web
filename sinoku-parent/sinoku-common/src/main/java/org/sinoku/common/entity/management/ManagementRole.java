e9577c06a1f2f2f36cec63e119d1cc32801016bb
shenao1991 <shenao19910806@outlook.com>
2017-07-02 23:23:37 +0800

    提交了角色model

---
+++ sinoku-parent/sinoku-common/src/main/java/org/sinoku/common/entity/management/ManagementRole.java
@@ -0,0 +1,42 @@
+package org.sinoku.common.entity.management;
+
+import org.sinoku.common.entity.common.CommonEntity;
+
+import java.io.Serializable;
+
+/**
+ * 运营角色
+ * Created by ao.shen on 2017/6/30.
+ */
+public class ManagementRole extends CommonEntity implements Serializable{
+
+    private Integer id;
+
+    private String roleName;
+
+    private String roleCode;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleCode() {
+        return roleCode;
+    }
+
+    public void setRoleCode(String roleCode) {
+        this.roleCode = roleCode;
+    }
+}
